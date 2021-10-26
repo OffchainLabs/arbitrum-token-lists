f67e1f6ecb31947745d560b5bedf1dcafdb1e69f
dzgoldman <dzgoldman@wesleyan.edu>
2021-10-26 09:53:49 -0400

    fix graph query cap

--- src/lib/graph.ts
+++ src/lib/graph.ts
@@ -1,32 +1,30 @@
-
-import { request, gql } from "graphql-request";
-import { excludeList } from "./utils";
+import { request, gql } from 'graphql-request';
+import { excludeList } from './utils';
 
 const apolloL2GatewaysRinkebyClient =
-  "https://api.thegraph.com/subgraphs/name/fredlacs/layer2-token-gateway-rinkeby";
+  'https://api.thegraph.com/subgraphs/name/fredlacs/layer2-token-gateway-rinkeby';
 const apolloL2GatewaysClient =
-  "https://api.thegraph.com/subgraphs/name/fredlacs/layer2-token-gateway";
+  'https://api.thegraph.com/subgraphs/name/fredlacs/layer2-token-gateway';
 
-
-const chaidIdToGraphClientUrl = (chainID:string)=>{
+const chaidIdToGraphClientUrl = (chainID: string) => {
   switch (chainID) {
     case '42161':
-      return apolloL2GatewaysClient
+      return apolloL2GatewaysClient;
     case '421611':
-    return apolloL2GatewaysRinkebyClient
+      return apolloL2GatewaysRinkebyClient;
     default:
-      throw new Error ("Unsupported chain");
+      throw new Error('Unsupported chain');
   }
-}
+};
 
 export const getTokens = async (
   l1TokenAddresses: string[],
   networkID: string
 ) => {
-  const clientUrl = chaidIdToGraphClientUrl(networkID)
+  const clientUrl = chaidIdToGraphClientUrl(networkID);
   const formattedAddresses = l1TokenAddresses
-    .map((a: string) => `"${a}"`)
-    .join(",");
+    .map((a: string) => `"${a}"`.toLowerCase())
+    .join(',');
   const query = gql`
   {
     tokens(where:{
@@ -41,17 +39,17 @@ export const getTokens = async (
   }
 `;
 
-  let { tokens } = await request(clientUrl, query);
+  const { tokens } = await request(clientUrl, query);
   return tokens.filter(
     (token: any) => !excludeList.includes(token.id.toLowerCase())
   );
 };
 
-export const getAllTokens = async (networkID:string) => {
-  const clientUrl = chaidIdToGraphClientUrl(networkID)
+export const getAllTokens = async (networkID: string) => {
+  const clientUrl = chaidIdToGraphClientUrl(networkID);
   const query = gql`
     {
-      tokens {
+      tokens(first: 500, skip: 0) {
         id
         l2Address
         gateway {
