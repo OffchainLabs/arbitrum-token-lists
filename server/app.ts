1d0dbe07db0677bb3a77755ed4aa5db5219ab7b2
dzgoldman <dzgoldman@wesleyan.edu>
2021-11-01 14:00:44 -0400

    add express server for full list

---
+++ server/app.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+const app = express();
+import dotenv from 'dotenv';
+import { readFileSync } from 'fs'
+dotenv.config();
+const port = process.env.PORT || 8080 
+
+
+app.get( "/", ( req, res ) => {
+    res.send( "ping" );
+} );
+
+app.get("/all_tokens", (req, res)=>{
+    const tokenData = readFileSync("./src/FullList/all_tokens.json")    
+
+    res.send(JSON.parse(tokenData.toString()))
+})
+
+app.listen( port, () => {
+    console.log( `server started at port ${ port }` );
+} );
