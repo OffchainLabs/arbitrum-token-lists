6d1da6f97171752a6a832528e9c13c638b9d06d1
fredlacs <32464905+fredlacs@users.noreply.github.com>
2022-06-09 12:39:43 +0100

    Update dependencies

--- src/lib/instantiate_bridge.ts
+++ src/lib/instantiate_bridge.ts
@@ -9,11 +9,23 @@ const networkID = args.l2NetworkID || 42161
 console.log('Using L2 networkID:', networkID);
 
 export const getNetworkConfig = async () => {
-  const l2Network = await getL2Network(networkID)
-  const l1Network = await getL1Network(l2Network.partnerChainID)
 
-  const arbProvider = new providers.JsonRpcProvider(l2Network.rpcURL);
-  const ethProvider = new providers.JsonRpcProvider(l1Network.rpcURL)
+  const l2Rpc = (() => {
+    if(networkID === 42161) return "https://arb1.arbitrum.io/rpc"
+    else if(networkID === 421611) return "https://rinkeby.arbitrum.io/rpc"
+    throw new Error("No L2 RPC detected")
+  })()
+  const arbProvider = new providers.JsonRpcProvider(l2Rpc);
+  const l2Network = await getL2Network(arbProvider)
+
+  const l1Rpc = (() => {
+    if(l2Network.partnerChainID === 1) return process.env['MAINNET_RPC'] as string
+    else if(l2Network.partnerChainID === 4) return process.env['RINKEBY_RPC'] as string
+    throw new Error("No L1 RPC detected")
+  })()
+  const ethProvider = new providers.JsonRpcProvider(l1Rpc)
+  const l1Network = await getL1Network(ethProvider)
+
 
   const l1MultiCaller = await MultiCaller.fromProvider(ethProvider)
   const l2MultiCaller = await MultiCaller.fromProvider(arbProvider)
