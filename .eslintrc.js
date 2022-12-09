71ad8a4d167e1e411b633605181d8457da6b13d0
Christophe <contact@christophedeveaux.com>
2022-12-09 13:28:49 +0100

    Fix formatting in .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,41 +4,41 @@ module.exports = {
     es6: true,
     node: true,
   },
-  ignorePatterns: ["dist"],
-  plugins: ["prettier"],
+  ignorePatterns: ['dist'],
+  plugins: ['prettier'],
   extends: [
-    "eslint:recommended",
-    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    'eslint:recommended',
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: "module", // Allows for the use of imports
+    sourceType: 'module', // Allows for the use of imports
   },
   rules: {
-    "prettier/prettier": "error",
-    "no-unused-vars": "warn",
-    "prefer-const": [2, { destructuring: "all" }],
-    "object-curly-spacing": ["error", "always"],
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'warn',
+    'prefer-const': [2, { destructuring: 'all' }],
+    'object-curly-spacing': ['error', 'always'],
   },
   overrides: [
     {
-      files: ["*.ts", "*.tsx"],
-      parser: "@typescript-eslint/parser",
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
       extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:prettier/recommended",
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
       ],
-      plugins: ["@typescript-eslint", "prettier"],
+      plugins: ['@typescript-eslint', 'prettier'],
       rules: {
-        "no-empty-pattern": "warn",
-        "no-useless-escape": "warn",
-        "prettier/prettier": ["error", { singleQuote: true }],
-        "@typescript-eslint/member-delimiter-style": ["off"],
-        "@typescript-eslint/no-explicit-any": ["warn"],
-        "@typescript-eslint/no-use-before-define": ["off"],
-        "@typescript-eslint/no-non-null-assertion": ["off"],
-        "@typescript-eslint/ban-ts-comment": ["warn"],
+        'no-empty-pattern': 'warn',
+        'no-useless-escape': 'warn',
+        'prettier/prettier': ['error', { singleQuote: true }],
+        '@typescript-eslint/member-delimiter-style': ['off'],
+        '@typescript-eslint/no-explicit-any': ['warn'],
+        '@typescript-eslint/no-use-before-define': ['off'],
+        '@typescript-eslint/no-non-null-assertion': ['off'],
+        '@typescript-eslint/ban-ts-comment': ['warn'],
       },
     },
   ],
