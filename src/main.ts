c6acffd148ea06b049b1c0a25720c43364ae8a60
Christophe <contact@christophedeveaux.com>
2022-12-09 13:15:56 +0100

    Fix prettier config to use singleQuote

--- src/main.ts
+++ src/main.ts
@@ -2,27 +2,27 @@ import {
   arbifyL1List,
   updateArbifiedList,
   generateFullList,
-} from "./lib/token_list_gen";
-import { addPermitTags } from "./PermitTokens/permitSignature";
-import args from "./lib/getClargs";
-import { ArbTokenList } from "./lib/types";
-import { writeToFile } from "./lib/store";
+} from './lib/token_list_gen';
+import { addPermitTags } from './PermitTokens/permitSignature';
+import args from './lib/getClargs';
+import { ArbTokenList } from './lib/types';
+import { writeToFile } from './lib/store';
 
 const main = async () => {
-  if (args.action === "full") {
-    if (args.tokenList !== "full")
+  if (args.action === 'full') {
+    if (args.tokenList !== 'full')
       throw new Error("expected --tokenList 'full'");
     if (args.includePermitTags)
-      throw new Error("full list mode does not support permit tagging");
+      throw new Error('full list mode does not support permit tagging');
 
     return writeToFile(await generateFullList());
   }
 
   let tokenList: ArbTokenList;
 
-  if (args.action === "arbify") {
+  if (args.action === 'arbify') {
     tokenList = await arbifyL1List(args.tokenList, !!args.includeOldDataFields);
-  } else if (args.action === "update") {
+  } else if (args.action === 'update') {
     tokenList = await updateArbifiedList(args.tokenList);
   } else {
     throw new Error(`action ${args.action} not recognised`);
@@ -34,7 +34,7 @@ const main = async () => {
 };
 
 main()
-  .then(() => console.log("Done."))
+  .then(() => console.log('Done.'))
   .catch(err => {
     console.error(err);
     throw err;
