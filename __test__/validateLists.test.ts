4c412805178ea7c53a64e77d3f5e22dba79e2642
Jason-Wanxt <wanxutao123@gmail.com>
2023-01-31 21:10:41 +0800

    resolve conflicts

--- __test__/validateLists.test.ts
+++ __test__/validateLists.test.ts
@@ -61,41 +61,41 @@ describe('Token Lists', () => {
   jest.setTimeout(800_000);
 
   describe('Arbify token lists', () => {
-    // it('Arb1 Uniswap', async () => {
-    //   expect.assertions(2);
-    //   const [localList, onlineList] = await Promise.all([
-    //     runCommand(Action.Arbify, [
-    //       '--l2NetworkID=42161',
-    //       '--tokenList=https://gateway.ipfs.io/ipns/tokens.uniswap.org',
-    //       '--ignorePreviousList=true',
-    //     ]),
-    //     fetch(
-    //       'https://tokenlist.arbitrum.io/ArbTokenLists/arbed_uniswap_labs_list.json'
-    //     ).then(response => response.json()),
-    //   ]);
-    //   testNoDuplicates(localList as ArbTokenList)
+    it('Arb1 Uniswap', async () => {
+      expect.assertions(2);
+      const [localList, onlineList] = await Promise.all([
+        runCommand(Action.Arbify, [
+          '--l2NetworkID=42161',
+          '--tokenList=https://gateway.ipfs.io/ipns/tokens.uniswap.org',
+          '--ignorePreviousList=true',
+        ]),
+        fetch(
+          'https://tokenlist.arbitrum.io/ArbTokenLists/arbed_uniswap_labs_list.json'
+        ).then(response => response.json()),
+      ]);
+      testNoDuplicates(localList as ArbTokenList)
       
 
-    //   compareLists(localList, onlineList);
-    // });
+      compareLists(localList, onlineList);
+    });
 
-    // it('Arb1 Gemini', async () => {
-    //   expect.assertions(2);
-    //   const [localList, onlineList] = await Promise.all([
-    //     runCommand(Action.Arbify, [
-    //       '--l2NetworkID=42161',
-    //       '--tokenList=https://www.gemini.com/uniswap/manifest.json',
-    //       '--ignorePreviousList=true',
-    //     ]),
-    //     fetch(
-    //       'https://tokenlist.arbitrum.io/ArbTokenLists/arbed_gemini_token_list.json'
-    //     ).then(response => response.json()),
-    //   ]);
+    it('Arb1 Gemini', async () => {
+      expect.assertions(2);
+      const [localList, onlineList] = await Promise.all([
+        runCommand(Action.Arbify, [
+          '--l2NetworkID=42161',
+          '--tokenList=https://www.gemini.com/uniswap/manifest.json',
+          '--ignorePreviousList=true',
+        ]),
+        fetch(
+          'https://tokenlist.arbitrum.io/ArbTokenLists/arbed_gemini_token_list.json'
+        ).then(response => response.json()),
+      ]);
 
-    //   testNoDuplicates(localList as ArbTokenList)
+      testNoDuplicates(localList as ArbTokenList)
 
-    //   compareLists(localList, onlineList);
-    // });
+      compareLists(localList, onlineList);
+    });
 
     it('Arb1 CMC', async () => {
       expect.assertions(2);
@@ -114,126 +114,126 @@ describe('Token Lists', () => {
       compareLists(localList, onlineList);
     });
 
-    // it('Arb Nova Uniswap', async () => {
-    //   expect.assertions(2);
-    //   const [localList, onlineList] = await Promise.all([
-    //     runCommand(Action.Arbify, [
-    //       '--l2NetworkID=42170',
-    //       '--tokenList=https://gateway.ipfs.io/ipns/tokens.uniswap.org',
-    //       '--ignorePreviousList=true',
-    //     ]),
-    //     fetch(
-    //       'https://tokenlist.arbitrum.io/ArbTokenLists/42170_arbed_uniswap_labs_default.json'
-    //     ).then(response => response.json()),
-    //   ]);      
+    it('Arb Nova Uniswap', async () => {
+      expect.assertions(2);
+      const [localList, onlineList] = await Promise.all([
+        runCommand(Action.Arbify, [
+          '--l2NetworkID=42170',
+          '--tokenList=https://gateway.ipfs.io/ipns/tokens.uniswap.org',
+          '--ignorePreviousList=true',
+        ]),
+        fetch(
+          'https://tokenlist.arbitrum.io/ArbTokenLists/42170_arbed_uniswap_labs_default.json'
+        ).then(response => response.json()),
+      ]);      
 
-    //   testNoDuplicates(localList as ArbTokenList)
+      testNoDuplicates(localList as ArbTokenList)
 
-    //   compareLists(localList, onlineList);
-    // });
+      compareLists(localList, onlineList);
+    });
 
-    // it('Arb Nova Gemini', async () => {
-    //   expect.assertions(2);
-    //   const [localList, onlineList] = await Promise.all([
-    //     runCommand(Action.Arbify, [
-    //       '--l2NetworkID=42170',
-    //       '--tokenList=https://www.gemini.com/uniswap/manifest.json',
-    //       '--ignorePreviousList=true',
-    //     ]),
-    //     fetch(
-    //       'https://tokenlist.arbitrum.io/ArbTokenLists/42170_arbed_gemini_token_list.json'
-    //     ).then(response => response.json()),
-    //   ]); 
-    //   testNoDuplicates(localList as ArbTokenList)
-    //   compareLists(localList, onlineList);
-    // });
+    it('Arb Nova Gemini', async () => {
+      expect.assertions(2);
+      const [localList, onlineList] = await Promise.all([
+        runCommand(Action.Arbify, [
+          '--l2NetworkID=42170',
+          '--tokenList=https://www.gemini.com/uniswap/manifest.json',
+          '--ignorePreviousList=true',
+        ]),
+        fetch(
+          'https://tokenlist.arbitrum.io/ArbTokenLists/42170_arbed_gemini_token_list.json'
+        ).then(response => response.json()),
+      ]); 
+      testNoDuplicates(localList as ArbTokenList)
+      compareLists(localList, onlineList);
+    });
 
-    // it('Arb Nova CMC', async () => {
-    //   expect.assertions(2);
-    //   const [localList, onlineList] = await Promise.all([
-    //     runCommand(Action.Arbify, [
-    //       '--l2NetworkID=42170',
-    //       '--tokenList=https://api.coinmarketcap.com/data-api/v3/uniswap/all.json',
-    //       '--ignorePreviousList=true',
-    //     ]),
-    //     fetch(
-    //       'https://tokenlist.arbitrum.io/ArbTokenLists/42170_arbed_coinmarketcap.json'
-    //     ).then(response => response.json()),
-    //   ]);
-    //   testNoDuplicates(localList as ArbTokenList)
-    //   compareLists(localList, onlineList);
-    // });
+    it('Arb Nova CMC', async () => {
+      expect.assertions(2);
+      const [localList, onlineList] = await Promise.all([
+        runCommand(Action.Arbify, [
+          '--l2NetworkID=42170',
+          '--tokenList=https://api.coinmarketcap.com/data-api/v3/uniswap/all.json',
+          '--ignorePreviousList=true',
+        ]),
+        fetch(
+          'https://tokenlist.arbitrum.io/ArbTokenLists/42170_arbed_coinmarketcap.json'
+        ).then(response => response.json()),
+      ]);
+      testNoDuplicates(localList as ArbTokenList)
+      compareLists(localList, onlineList);
+    });
 
-    // it('Arb Goerli CMC', async () => {
-    //   expect.assertions(2);
-    //   const [localList, onlineList] = await Promise.all([
-    //     runCommand(Action.Arbify, [
-    //       '--l2NetworkID=421613',
-    //       '--tokenList=https://api.coinmarketcap.com/data-api/v3/uniswap/all.json',
-    //       '--ignorePreviousList=true',
-    //     ]),
-    //     fetch(
-    //       'https://tokenlist.arbitrum.io/ArbTokenLists/421613_arbed_coinmarketcap.json'
-    //     ).then(response => response.json()),
-    //   ]);
-    //   testNoDuplicates(localList as ArbTokenList)
-    //   compareLists(localList, onlineList);
-    // });
+    it('Arb Goerli CMC', async () => {
+      expect.assertions(2);
+      const [localList, onlineList] = await Promise.all([
+        runCommand(Action.Arbify, [
+          '--l2NetworkID=421613',
+          '--tokenList=https://api.coinmarketcap.com/data-api/v3/uniswap/all.json',
+          '--ignorePreviousList=true',
+        ]),
+        fetch(
+          'https://tokenlist.arbitrum.io/ArbTokenLists/421613_arbed_coinmarketcap.json'
+        ).then(response => response.json()),
+      ]);
+      testNoDuplicates(localList as ArbTokenList)
+      compareLists(localList, onlineList);
+    });
   });
 
-  // describe('Update token lists', () => {
-  //   it('should return the same list as the online version', async () => {
-  //     expect.assertions(2);
-  //     const [localList, onlineList] = await Promise.all([
-  //       runCommand(Action.Update, [
-  //         '--l2NetworkID=42161',
-  //         '--tokenList=https://tokenlist.arbitrum.io/ArbTokenLists/arbed_arb_whitelist_era.json',
-  //         '--includeOldDataFields=true',
-  //       ]),
-  //       fetch(
-  //         'https://tokenlist.arbitrum.io/ArbTokenLists/arbed_arb_whitelist_era.json'
-  //       ).then(response => response.json()),
-  //     ]);
-  //     testNoDuplicates(localList as ArbTokenList)
+  describe('Update token lists', () => {
+    it('should return the same list as the online version', async () => {
+      expect.assertions(2);
+      const [localList, onlineList] = await Promise.all([
+        runCommand(Action.Update, [
+          '--l2NetworkID=42161',
+          '--tokenList=https://tokenlist.arbitrum.io/ArbTokenLists/arbed_arb_whitelist_era.json',
+          '--includeOldDataFields=true',
+        ]),
+        fetch(
+          'https://tokenlist.arbitrum.io/ArbTokenLists/arbed_arb_whitelist_era.json'
+        ).then(response => response.json()),
+      ]);
+      testNoDuplicates(localList as ArbTokenList)
 
-  //     compareLists(localList, onlineList);
-  //   });
-  // });
+      compareLists(localList, onlineList);
+    });
+  });
 
-  // describe('fullList', () => {
-  //   it('should generate fullList for a given network', async () => {
-  //     expect.assertions(1);
-  //     const [localList, onlineList] = await Promise.all([
-  //       runCommand(Action.Full, [
-  //         '--l2NetworkID=42161',
-  //         '--tokenList=full',
-  //         '--ignorePreviousList=true',
-  //       ]),
-  //       fetch('https://tokenlist.arbitrum.io/FullList/all_tokens.json').then(
-  //         response => response.json()
-  //       ),
-  //     ]);
+  describe('fullList', () => {
+    it('should generate fullList for a given network', async () => {
+      expect.assertions(1);
+      const [localList, onlineList] = await Promise.all([
+        runCommand(Action.Full, [
+          '--l2NetworkID=42161',
+          '--tokenList=full',
+          '--ignorePreviousList=true',
+        ]),
+        fetch('https://tokenlist.arbitrum.io/FullList/all_tokens.json').then(
+          response => response.json()
+        ),
+      ]);
 
-  //     compareLists(localList, onlineList);
-  //   });
-  // });
+      compareLists(localList, onlineList);
+    });
+  });
 
-  // describe('allTokensList', () => {
-  //   it('should generate allTokensList for a given network', async () => {
-  //     expect.assertions(2);
-  //     const [localList, onlineList] = await Promise.all([
-  //       runCommand(Action.AllTokensList, [
-  //         '--l2NetworkID=421613',
-  //         '--tokenList=full',
-  //         '--ignorePreviousList=true',
-  //       ]),
-  //       fetch(
-  //         'https://tokenlist.arbitrum.io/ArbTokenLists/421613_arbed_full.json'
-  //       ).then(response => response.json()),
-  //     ]);
-  //     testNoDuplicates(localList as ArbTokenList)
+  describe('allTokensList', () => {
+    it('should generate allTokensList for a given network', async () => {
+      expect.assertions(2);
+      const [localList, onlineList] = await Promise.all([
+        runCommand(Action.AllTokensList, [
+          '--l2NetworkID=421613',
+          '--tokenList=full',
+          '--ignorePreviousList=true',
+        ]),
+        fetch(
+          'https://tokenlist.arbitrum.io/ArbTokenLists/421613_arbed_full.json'
+        ).then(response => response.json()),
+      ]);
+      testNoDuplicates(localList as ArbTokenList)
 
-  //     compareLists(localList, onlineList);
-  //   });
-  // });
+      compareLists(localList, onlineList);
+    });
+  });
 });
