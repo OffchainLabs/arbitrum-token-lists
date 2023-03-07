9b7b78cfa6095eb7dd647fa75d60ace2b7cec4ae
Christophe Deveaux <cdeveaux@offchainlabs.com>
2023-03-07 11:45:05 +0100

    fix: update uniswap token list url to avoid rate-limiting (#79)

--- __test__/integration/validateLists.test.ts
+++ __test__/integration/validateLists.test.ts
@@ -69,7 +69,7 @@ describe('Token Lists', () => {
       const [localList, onlineList] = await Promise.all([
         runCommand(Action.Arbify, [
           '--l2NetworkID=42161',
-          '--tokenList=https://gateway.ipfs.io/ipns/tokens.uniswap.org',
+          '--tokenList=https://tokens.uniswap.org',
           '--ignorePreviousList=true',
         ]),
         fetch(
@@ -120,7 +120,7 @@ describe('Token Lists', () => {
       const [localList, onlineList] = await Promise.all([
         runCommand(Action.Arbify, [
           '--l2NetworkID=42170',
-          '--tokenList=https://gateway.ipfs.io/ipns/tokens.uniswap.org',
+          '--tokenList=https://tokens.uniswap.org',
           '--ignorePreviousList=true',
         ]),
         fetch(
