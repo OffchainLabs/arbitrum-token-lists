512b7e50319c98bd03cf949ed8aa132612ace375
Jason-Wanxt <wanxutao123@gmail.com>
2023-03-09 17:46:18 +0800

    remove getPath

--- src/commands/allTokensList.ts
+++ src/commands/allTokensList.ts
@@ -1,7 +1,7 @@
 import { ArbTokenList } from '../lib/types';
 import { generateFullListFormatted } from '../lib/token_list_gen';
 import { addPermitTags } from '../PermitTokens/permitSignature';
-import { writeToFile, getPath } from '../lib/store';
+import { writeToFile } from '../lib/store';
 import { Action, Args } from '../lib/options';
 
 export const command = Action.AllTokensList;
@@ -9,10 +9,12 @@ export const command = Action.AllTokensList;
 export const describe = 'All tokens list';
 
 export const handler = async (argvs: Args) => {
+  if(!argvs.newArbifiedList) {
+    throw new Error("Action allTokenList needs --newArbifiedList flag")
+  }
+  
   let tokenList: ArbTokenList = await generateFullListFormatted();
-  const path = getPath('full');
-
   if (argvs.includePermitTags) tokenList = await addPermitTags(tokenList);
-  writeToFile(tokenList, path);
+  writeToFile(tokenList, argvs.newArbifiedList);
   return tokenList;
 };
