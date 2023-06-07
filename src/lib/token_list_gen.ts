d041cc2beba222893e3826e3bef70f9646e5d4aa
Christophe <contact@christophedeveaux.com>
2023-06-07 12:25:41 +0200

    feat: Update USDC symbol for bridged USDC

--- src/lib/token_list_gen.ts
+++ src/lib/token_list_gen.ts
@@ -37,6 +37,7 @@ import { readFileSync, existsSync } from 'fs';
 import { getNetworkConfig } from './instantiate_bridge';
 import { getPrevList } from './store';
 import { getArgvs } from './options';
+import { USDCContractAddressArb1 } from './constants';
 
 export interface ArbificationOptions {
   overwriteCurrentList: boolean;
@@ -219,7 +220,9 @@ export const generateTokenList = async (
         // parse null terminated bytes32 strings
         else if (_name.length === 64)
           return utils.parseBytes32String('0x' + _name);
-        else return _name;
+        else if (token.l2Address === USDCContractAddressArb1) {
+          return 'Bridged USDC (Arb1)';
+        } else return _name;
       })();
 
       _symbol = (() => {
@@ -233,7 +236,9 @@ export const generateTokenList = async (
         // parse null terminated bytes32 strings
         else if (_symbol.length === 64)
           return utils.parseBytes32String('0x' + _symbol);
-        else return _symbol;
+        else if (token.l2Address === USDCContractAddressArb1) {
+          return 'USDC.e';
+        } else return _symbol;
       })();
 
       const name = sanitizeNameString(_name);
