60810fcbb0d6308ad43e242fe39c9ab13ade7422
Christophe <contact@christophedeveaux.com>
2024-01-03 19:54:38 +0100

    Increase timeout

--- __test__/integration/validateLists.test.ts
+++ __test__/integration/validateLists.test.ts
@@ -61,7 +61,7 @@ const testNoDuplicates = (arbTokenList: ArbTokenList) => {
 };
 
 describe('Token Lists', () => {
-  jest.setTimeout(200_000);
+  jest.setTimeout(300_000);
 
   describe('Arbify token lists', () => {
     describe('Arb1', () => {
