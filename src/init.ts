694b4b40acfa1b303480f10c88b2763823f0aa7a
Christophe <contact@christophedeveaux.com>
2024-11-26 19:30:58 +0000

    refactor: Generate matrix from custom networks

--- src/init.ts
+++ src/init.ts
@@ -1,6 +1,11 @@
 import * as dotenv from 'dotenv';
 import dotenvExpand from 'dotenv-expand';
-import './customNetworks';
+import { customNetworks } from './customNetworks';
+import { addCustomNetwork } from '@arbitrum/sdk';
+
+Object.values(customNetworks).forEach((network) => {
+  addCustomNetwork({ customL2Network: network });
+});
 
 const myEnv = dotenv.config();
 dotenvExpand.expand(myEnv);
