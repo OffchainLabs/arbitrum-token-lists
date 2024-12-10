c8f79b59abf05fee18c558e05340fac0f79c3c9e
Christophe <contact@christophedeveaux.com>
2024-12-10 22:26:33 +0000

    Add support for base, baseSepolia, holesky

--- src/customNetworks.ts
+++ src/customNetworks.ts
@@ -8,7 +8,7 @@ const orbitChainsRpc = orbitChainsData.data.reduce((acc, chain) => {
   return acc;
 }, {} as Record<number, string>);
 
-export const rpcs = {
+export const rpcs: Record<number, string> = {
   // Arbitrum networks
   42161: 'https://arb1.arbitrum.io/rpc',
   42170: 'https://nova.arbitrum.io/rpc',
