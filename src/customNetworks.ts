3ef632b4d78920b90068390082f7200dd0489e0a
Christophe <contact@christophedeveaux.com>
2025-09-17 09:14:24 +0000

    chore: Remove wylerchain sepolia from excluded networks

--- src/customNetworks.ts
+++ src/customNetworks.ts
@@ -1,7 +1,7 @@
 import { ArbitrumNetwork } from '@arbitrum/sdk';
 import orbitChainsData from './Assets/orbitChainsData.json';
 
-const excludedNetworksIds: number[] = [98867, 1712]; // Plume testnet, Wylerchain Sepolia
+const excludedNetworksIds: number[] = [98867]; // Plume testnet
 
 export const customNetworks = (
   orbitChainsData.data as ArbitrumNetwork[]
